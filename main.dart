1d58efc0eee9facc63b5241adc19789b2af7185d
MalekIrsheid <malekirsheid2000@gmail.com>
2022-08-28 13:29:59 +0300

    ASS1 update

--- main.dart
+++ main.dart
@@ -1,9 +1,10 @@
 void main() {
   List myColors = ["black", "white", "pink", "red", "aqua"];
   for (var i = 0; i < myColors.length; i++) {
-    if (myColors[i].toString().contains("a") ||
-        myColors[i].toString().contains("b")) {
-      print(myColors[i].toString().toUpperCase());
+    String colors = myColors[i];
+    if (colors[0] == 'a' || colors[0] == 'b') {
+      String color1 = colors.toUpperCase();
+      print(color1.toUpperCase());
     } else {
       print(myColors[i] + " " + "sorry this colors not start with a,b");
     }
